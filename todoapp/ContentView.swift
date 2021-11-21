32842028a9cf1f1011ac61546d8c08dc03aa3d35
rizkyalviandra <rizkyalviandra@gmail.com>
2021-11-21 11:12:02 +0700

    Initial Commit

---
+++ todoapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todoapp
+//
+//  Created by Rizky Alviandra on 21/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by Rizky Alviandra on 21/11/21.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
